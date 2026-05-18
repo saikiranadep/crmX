ef881b52f8808dfdaec1994237b74fee9cb42585
Saikiran Adep <saikiranaspire@gmail.com>
2026-05-18 21:07:06 +0530

    Bug fix deatil routing-tooltip on remarks-

--- src/app/features/leads/pages/leads-detail/leads-detail.component.ts
+++ src/app/features/leads/pages/leads-detail/leads-detail.component.ts
@@ -3,35 +3,60 @@ import {
   ActivatedRoute,
   Router
 } from '@angular/router';
-import { DatePipe } from '@angular/common';
+import { CommonModule,DatePipe } from '@angular/common';
 
 import { LeadsService } from '../../services/leads.service';
 
 @Component({
   selector: 'app-leads-detail',
   standalone: true,
-  imports: [DatePipe],
+  imports: [CommonModule,DatePipe],
   templateUrl: './leads-detail.component.html'
 })
 export class LeadsDetailComponent {
-
   private readonly route = inject(ActivatedRoute);
 
   private readonly router = inject(Router);
 
   readonly leadsService = inject(LeadsService);
 
-  readonly id =
-    Number(this.route.snapshot.paramMap.get('id'));
+  // readonly id =
+  // Number(this.route.snapshot.paramMap.get('id'));
 
-  readonly lead =
-    this.leadsService.getLeadById(this.id);
+  // lead: any;
 
+  // ngOnInit(): void {
+
+  //   this.lead =
+  //     this.leadsService.getLeadById(this.id);
+  //     console.log("DeatilLead",this.lead);
+
+  // }
+  // readonly lead =
+  //   this.leadsService.getLeadById(this.id);
+
+  lead: any;
+  leadId = 0;
+  ngOnInit(): void {
+
+    this.route.paramMap.subscribe(params => {
+
+      this.leadId =
+        Number(params.get('id'));
+
+      this.lead =
+        this.leadsService.getLeadById(this.leadId);
+
+      // console.log('DetailLead', this.lead);
+
+    });
+
+  }
   editLead(): void {
 
     this.router.navigate([
       '/app/leads',
-      this.id,
+      this.leadId,
       'edit'
     ]);
 
